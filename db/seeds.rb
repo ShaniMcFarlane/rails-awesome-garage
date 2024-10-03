5283694f7f6b3f999301f5ea9932aa5456dff7fe
shani.mcfarlane96@gmail.com <shani.mcfarlane96@gmail.com>
2024-10-03 11:56:54 +0100

    created models --> migrated and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Car.destroy_all
+Owner.destroy_all
+
+owner_a = Owner.create(nickname: "A")
+owner_b = Owner.create(nickname: "B")
+owner_c = Owner.create(nickname: "C")
+
+Car.create(owner: owner_c, brand: "Porsche", model: "914", year: 1970, fuel: "Petrol")
+Car.create(owner: owner_a, brand: "Aston Martin", model: "Zagato", year: 1960, fuel: "Petrol")
+Car.create(owner: owner_b, brand: "Bentley", model: "MKV", year: 1940, fuel: "Petrol")
+
+puts "#{Car.all.count} Cars and #{Owner.all.count} owners"
